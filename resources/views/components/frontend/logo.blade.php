f6e2f7228639cd382f0aa2350b1906430046c385
Abdul Aziz Nurfarhana <abaz.nurfarhana@gmail.com>
2021-10-26 09:43:47 +0700

    Fix Logo Path

--- resources/views/components/frontend/logo.blade.php
+++ resources/views/components/frontend/logo.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ $attributes->get('href') ?? '#' }}" class="logo d-flex align-items-center">
-    <img src="{{ $attributes->get('logo') ?? asset(config('laraview.assets.url_prefix') . '/laraview/assets/img/logo-mdigi.png') }}"
+    <img src="{{ $attributes->get('logo') ?? asset(config('laraview.assets.url_prefix') . '/mdigi/laraview/assets/img/logo-mdigi.png') }}"
          class="gambar" style="opacity: .8; width: 20px;">
     <span>{{ config('app.name', 'MDIGI Laraview') }}</span>
 </a>
